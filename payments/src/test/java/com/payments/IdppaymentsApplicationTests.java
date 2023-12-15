ca111ca9d4d2849f54e0f9dc1ffb58aa385c2380
Dante Lacerda <dantelacerda01@gmail.com>
2023-12-15 20:50:09 -0300

    Base commit of the project
    Base classes to the project

---
+++ payments/src/test/java/com/payments/IdppaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdppaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
